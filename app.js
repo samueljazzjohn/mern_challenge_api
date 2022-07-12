8010556009de76670242e12b7494fb10ad74997f
samueljazzjohn <samueljazzjohn@gmail.com>
2022-07-12 17:13:45 +0530

    task2 completed

--- app.js
+++ app.js
@@ -5,16 +5,12 @@ var indexRouter = require('./routes/index');
 var mongoose = require('mongoose');
 var app = express();
 var cors=require('cors')
-// const dotenv = require('dotenv');
-// dotenv.config();
 
 app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-// const uri = process.env.MONGODB_URI
 
 mongoose.connect('mongodb+srv://jazzjohn:Jazz2%401999@where2buy.qgezx.mongodb.net/mern_challenge?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true },(err,db)=>{
     if(err) throw err;
